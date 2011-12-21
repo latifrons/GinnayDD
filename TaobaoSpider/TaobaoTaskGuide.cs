7ce93633835726d95bca14f9e69891ecd62c9144
latifrons <latifrons88@gmail.com>
2011-12-21 22:41:28 +0800

    HandleProviderRate
    HandleProviderList

--- TaobaoSpider/TaobaoTaskGuide.cs
+++ TaobaoSpider/TaobaoTaskGuide.cs
@@ -21,6 +21,9 @@ namespace TaobaoSpider
 
 		static Regex RegexSellerID = new Regex(@"user_number_id=(?<number>\d+)");
 		static Regex RegexUniqID = new Regex(@"uniqpid=(?<number>-?\d+)");
+		static Regex RegexCreateTime = new Regex(@"创店时间.+(?<time>\d{4}-\d{2}-\d{2})");
+
+		static Regex RegexEmpty = new Regex(@"\s+");
 
 		private static string UNIQ_ID = @"uniqID=";
 
@@ -28,11 +31,11 @@ namespace TaobaoSpider
 
 		private string storePath;
 		private string connectionString;
-//		public TaobaoTaskGuide(string storePath, string connectionString)
-//		{
-//			this.storePath = storePath;
-//			this.connectionString = connectionString;
-//		}
+		//		public TaobaoTaskGuide(string storePath, string connectionString)
+		//		{
+		//			this.storePath = storePath;
+		//			this.connectionString = connectionString;
+		//		}
 		public override string GetLocalStorePath(Task task)
 		{
 			return null;
@@ -76,7 +79,7 @@ namespace TaobaoSpider
 			TaskProcess tp = new TaskProcess();
 			tp.TaskData = td;
 			tp.CpResult = cpr;
-			
+
 			switch ((TaobaoTaskType)td.Task.Type)
 			{
 				case TaobaoTaskType.COMBINED_LIST:
@@ -96,12 +99,282 @@ namespace TaobaoSpider
 
 		private void HandleProviderRate(TaskProcess tp)
 		{
-			throw new NotImplementedException();
+			string html = Encoding.Default.GetString(tp.TaskData.Bytes);
+			if (!Validate(html))
+			{
+				tp.CpResult.Success = false;
+				return;
+			}
+			HtmlNode root = GetRoot(html);
+			if (root == null)
+			{
+				FailProcess(tp);
+				return;
+			}
+			Seller s = new Seller();
+			if (tp.TaskData.Task.Context == null)
+			{
+				LogMissing("TaobaoID", tp.TaskData.Task.Context);
+				tp.CpResult.Success = false;
+				return;
+			}
+			s.TaobaoId = Int32.Parse(tp.TaskData.Task.Context);
+			string credit = HAH.SafeGetSuccessorInnerText(root, @".//ul[@class='sep']/li/text()");
+			if (string.IsNullOrEmpty(credit))
+			{
+				LogMissing("credit",credit);
+				tp.CpResult.Success = false;
+				return;
+			}
+			credit = RegexEmpty.Replace(credit, "");
+			//卖家信用：5
+			s.Credit = int.Parse(credit.Substring(5));
+			string goodRate = HAH.SafeGetSuccessorInnerText(root, @".//div[@id='seller-rate']//em");
+			//好评率：98.30%
+			s.Goodrate = double.Parse(goodRate.Substring(4, goodRate.IndexOf("%") - 4));
+			s.IsTmall = root.SelectSingleNode(@".//div[@class='tmall-pro']") != null;
+
+			HtmlNodeCollection infos = root.SelectNodes(@".//div[@class='bd']/ul/li");
+			foreach (HtmlNode node in infos)
+			{
+				Match m = RegexCreateTime.Match(node.InnerText);
+				if (m.Success)
+				{
+					string date = m.Groups["time"].Value;
+					s.StartTime = DateTime.Parse(date);
+				}
+			}
+			
+			
+
+			//半年动态评分
+			{
+				HtmlNodeCollection nodes = root.SelectNodes(@".//div[@id='sixmonth']//div[@class='item-scrib']");
+				foreach (var node in nodes)
+				{
+					string title = HAH.SafeGetSuccessorInnerText(node, @"./span[@class='title']");
+					string count = HAH.SafeGetSuccessorInnerText(node, @"./em[@class='count']");
+					HtmlNode percent = node.SelectSingleNode(@".//strong[contains(@class),'percent']");
+					if (percent == null)
+					{
+						LogMissing(title,"Percent");
+						continue;
+					}
+
+					double c;
+					if (!double.TryParse(count, out c))
+					{
+						LogMissing(title,count);
+						continue;
+					}
+
+					double d;
+					string rate = percent.InnerText;
+					if (rate=="----")
+					{
+						d = 0;
+					}else if (!double.TryParse(rate,out d))
+					{
+						LogMissing(title,rate);
+						continue;
+					}
+					string percentClass = percent.Attributes["class"].Value;
+					if (percentClass.Contains("lower"))
+					{
+						d *= -1.0;
+					}
+					
+					if (title == "宝贝与描述相符：")
+					{
+						s.Rmatch = c;
+						s.Pmatch = d;
+					}
+					else if (title == "卖家的服务态度：")
+					{
+						s.Rservice = c;
+						s.Pservice = d;
+					}
+					else if (title == "卖家发货的速度：")
+					{
+						s.Rspeed = c;
+						s.Pspeed = d;
+					}
+				}
+			}
+
+			//30天服务
+			{
+				HtmlNodeCollection nodes = root.SelectNodes(@".//div[@class='each']");
+				foreach (var node in nodes)
+				{
+					HtmlNodeCollection innerNodes = node.SelectNodes(@"./span");
+					if (innerNodes.Count == 4)
+					{
+						string title = innerNodes[0].InnerText;
+						if (title.Contains("平均退款速度"))
+						{
+							s.Refunddays = Double.Parse(innerNodes[1].InnerText.Replace("%", ""));
+						}
+						else if (title.Contains("近30天退款率"))
+						{
+							s.Refundrate = Double.Parse(innerNodes[1].InnerText.Replace("%", ""));
+						}
+						else if (title.Contains("近30天投诉率"))
+						{
+							s.Complaint = Double.Parse(innerNodes[1].InnerText.Replace("%", ""));
+						}
+						else if (title.Contains("近30天处罚数"))
+						{
+							//0 次
+							s.Penalty = Int32.Parse(innerNodes[1].InnerText.Replace(" 次", ""));
+						}
+					}
+				}
+			}
 		}
 
 		private void HandleProviderList(TaskProcess tp)
 		{
-			throw new NotImplementedException();
+			string html = Encoding.Default.GetString(tp.TaskData.Bytes);
+			if (!Validate(html))
+			{
+				tp.CpResult.Success = false;
+				return;
+			}
+
+			HtmlNode root = GetRoot(html);
+			if (root == null)
+			{
+				FailProcess(tp);
+				return;
+			}
+			HtmlNodeCollection list = root.SelectNodes(@"//div[@id='list-content']//li[@class='list-item']");
+			if (list == null)
+			{
+				FailProcess(tp);
+				return;
+			}
+			bool success = true;
+			foreach (HtmlNode node in list)
+			{
+				success &= HandleProviderListNode(tp, node);
+				if (!success)
+				{
+					break;
+				}
+			}
+			tp.CpResult.Success = success;
+		}
+
+		private bool HandleProviderListNode(TaskProcess tp, HtmlNode node)
+		{
+			#region build an item
+			Item i = new Item();
+
+			string freight = HAH.SafeGetSuccessorInnerText(node, @".//li[@class='price']/span[@class='shipping']");
+			if (freight != null && freight.Length > 3)
+			{
+				//运费：8.00
+				string freightD = freight.Substring(3);
+				double d;
+				if (double.TryParse(freightD, out d))
+				{
+					i.Freight = d;
+				}
+				else
+				{
+					LogMissing("freight", freightD);
+					return false;
+				}
+			}
+			else
+			{
+				LogMissing("freight", freight);
+				return false;
+			}
+
+			i.Name = HAH.SafeGetSuccessorAttributeStringValue(node, @".//a[@class='EventCanSelect']", "title");
+			if (i.Name == null)
+			{
+				LogMissing("Name", node.InnerHtml);
+				return false;
+			}
+			i.Location = HAH.SafeGetSuccessorInnerText(node, @".//li[@class='place']/div[@class='loc']");
+			if (i.Location == null)
+			{
+				LogMissing("Location", node.InnerHtml);
+				return false;
+			}
+			string price = HAH.SafeGetSuccessorInnerText(node, @".//li[@class='price']/em");
+			if (!string.IsNullOrEmpty(price))
+			{
+				//359.00
+				double d;
+				if (double.TryParse(price, out d))
+				{
+					i.Price = d;
+				}
+				else
+				{
+					LogMissing("price", price);
+					return false;
+				}
+			}
+			else
+			{
+				LogMissing("price", price);
+				return false;
+			}
+
+			i.RecentDeal = 0;
+			string recentDeal = HAH.SafeGetSuccessorInnerText(node, @".//li[@class='sale']/text()");
+
+			if (!string.IsNullOrEmpty(recentDeal))
+			{
+				Match m = RegexRecentSellCount.Match(recentDeal);
+				if (m.Success)
+				{
+					i.RecentDeal = Int32.Parse(m.Groups["number"].Value);
+				}
+			}
+
+			string sellerID = HAH.SafeGetSuccessorAttributeStringValue(node, @".//p[contains(@class,'seller')]/a", "href");
+
+			if (!string.IsNullOrEmpty(sellerID))
+			{
+				Match m = RegexSellerID.Match(sellerID);
+				if (m.Success)
+				{
+					i.SellerTaobaoId = Int32.Parse(m.Groups["number"].Value);
+				}
+				else
+				{
+					LogMissing("SellerID", sellerID);
+					return false;
+				}
+			}
+			else
+			{
+				LogMissing("SellerID", node.InnerHtml);
+				return false;
+			}
+
+			i.UniqId = 0;
+			i.UrlLink = HAH.SafeGetSuccessorAttributeStringValue(node, ".//a[@class='EventCanSelect']", "href");
+
+			OpsItem.Insert(i);
+			#endregion
+			#region build new task
+			//Seller
+			tp.CpResult.NewTasks.Add(new Task
+			{
+				Url = RateURL.Replace("#UID#", i.SellerTaobaoId.ToString()),
+				Type = (int)TaobaoTaskType.PROVIDER_RATE,
+				Context = i.SellerTaobaoId.ToString()
+			});
+			#endregion
+
+			return true;
 		}
 
 		private void HandleCombinedList(TaskProcess tp)
@@ -113,7 +386,6 @@ namespace TaobaoSpider
 				return;
 			}
 
-			HtmlDocument doc = new HtmlDocument();
 			HtmlNode root = GetRoot(html);
 			if (root == null)
 			{
@@ -274,10 +546,11 @@ namespace TaobaoSpider
 			#region build new task
 			//Seller
 			tp.CpResult.NewTasks.Add(new Task
-			                         	{
-											Url = RateURL.Replace("#UID#",i.SellerTaobaoId.ToString()),
+										{
+											Url = RateURL.Replace("#UID#", i.SellerTaobaoId.ToString()),
 											Type = (int)TaobaoTaskType.PROVIDER_RATE,
-			                         	});
+											Context = sellerID
+										});
 			#endregion
 
 			return true;
@@ -287,19 +560,19 @@ namespace TaobaoSpider
 			string link = HAH.SafeGetSuccessorAttributeStringValue(node, @".//div[@class='legend2']/a", "href");
 			if (link == null)
 			{
-				LogMissing("URL",node.InnerHtml);
+				LogMissing("URL", node.InnerHtml);
 				return false;
 			}
 			Match m = RegexUniqID.Match(link);
-			
+
 			if (m.Success)
 			{
 				string uniqIDs = m.Groups["number"].Value;
 				tp.CpResult.NewTasks.Add(new Task
 				{
 					Type = (int)TaobaoTaskType.PROVIDER_LIST,
-					Url = FixRelativeURL(link,tp.TaskData.Task.Host),
-					Context = UNIQ_ID + uniqIDs,
+					Url = FixRelativeURL(link, tp.TaskData.Task.Host),
+					Context = uniqIDs,
 				});
 			}
 			return true;
